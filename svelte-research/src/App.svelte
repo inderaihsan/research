bf74c3f3eb4a4a36cd26147c0946932c3d7c48f0
inderaihsan <inderaihsan>
2025-11-14 01:17:11 +0700

    add svelte

---
+++ svelte-research/src/App.svelte
@@ -0,0 +1,27 @@
+<script>
+  import svelteLogo from "./assets/svelte.svg";
+  import viteLogo from "/vite.svg";
+  import Counter from "./lib/Counter.svelte";
+  import NewComponent from "./lib/Bored.svelte";
+  import Map from "./lib/Map.svelte";
+  import Searchbar from "./lib/Searchbar.svelte";
+</script>
+
+<main>
+  <h1>Goval</h1>
+  <NewComponent onincrement={() => console.log("Count")} />
+  <div class="search-bar">
+    <Searchbar />
+  </div>
+  <div class="map">
+    <Map height="650px"></Map>
+  </div>
+</main>
+
+<style>
+  .map {
+    height: 90em;
+    width: 80em;
+    padding: 1.5em;
+  }
+</style>
